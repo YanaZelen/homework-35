ed4ce9f900b805c38133860bbb4f19d981799e74
YanaZelen <104030970+YanaZelen@users.noreply.github.com>
2023-06-02 17:32:38 +0300

    HW35

--- src/main/java/ru/maxima/homework35/config/SpringSecurityConfig.java
+++ src/main/java/ru/maxima/homework35/config/SpringSecurityConfig.java
@@ -2,7 +2,6 @@ package ru.maxima.homework35.config;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.InternalAuthenticationServiceException;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -17,7 +16,6 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
 
   @Override
   protected void configure(HttpSecurity http) throws Exception{
-    try {
       http
               .authorizeRequests()
               .antMatchers("/").permitAll()
